e0f9f9bec2af921cbaa0d5186cf4b77db02585f1
Junho <wnsgh9808@naver.com>
2024-08-09 16:00:33 +0900

    init

---
+++ src/main/java/com/example/traveltile/TravelTileApplication.java
@@ -0,0 +1,13 @@
+package com.example.traveltile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/traveltile/TravelTileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traveltile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
